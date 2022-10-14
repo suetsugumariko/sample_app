d8d3483327253316ea381ef108e9dfc34f162542
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2022-10-14 04:33:34 +0000

    [Finish]3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,10 +34,8 @@ class ListsController < ApplicationController
 
 
   private
-
-
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
